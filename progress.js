d535aa9196aeda19db07eff37652715222252f05
donnieSky <donniesky@users.noreply.github.com>
2020-12-22 16:29:54 +0800

    Update progress.js

--- progress.js
+++ progress.js
@@ -15,11 +15,26 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### Hi there 👋
+### Hi there <img src='https://qpluspicture.oss-cn-beijing.aliyuncs.com/6LjjQA/Hi.gif' alt='Hi' width="24"/>
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
+
+![donniesky's github stats](https://bad-apple-github-readme.vercel.app/api?show_bg=1&username=donniesky&show_icons=true&theme=dracula)
+
+- 🔭 I’m currently working on Weeget
+- 🎓 I’m graduated at Renmin University of China
+- 🌱 I’m currently learning Flutter
+- 👯 I’m looking to collaborate on anything about Android
+- 💬 Ask me about everything you want talk with me
+- 📫 How to reach me: https://t.me/donnieSky
+- 😄 Pronouns: Action speak louder than words!
+- ⚡ Fun fact: 👫 🐶 🐈 :octocat: 🏀 🚴 🎮 :hearts: 🍚 ✈️
+
+[<img src="https://img.shields.io/badge/Twitter-%40donniesky-blue">](https://twitter.com/donnieSky815)
+[<img src="https://img.shields.io/badge/Email-donniesky.me%40gmail.com-orange">](mailto:donniesky.me@gmail.com)
+
 ---
 ⏰ Updated on ${new Date().toUTCString()}
-![Progress Bar CI](https://github.com/donniesky/donniesky/workflows/Progress%20Bar%20CI/badge.svg)\
-`
+![Progress Bar CI](https://github.com/donniesky/donniesky/workflows/Progress%20Bar%20CI/badge.svg)
+\`
 
 console.log(readme)
