7b86b2eb52e22190c86c7f9a6692f073001db990
donnieSky <donniesky@users.noreply.github.com>
2020-12-23 11:34:59 +0800

    Update build_README.py

--- build_README.py
+++ build_README.py
@@ -7,6 +7,7 @@ import re
 import os
 import datetime
 import logging
+import pendulum
 
 root = pathlib.Path(__file__).parent.resolve()
 client = GraphqlClient(endpoint="https://api.github.com/graphql")
@@ -33,15 +34,45 @@ def fetch_code_time():
         "https://gist.github.com/donniesky/f1cb72d6b3792a01b11ad06820022053/raw/"
     )
 
+def get_year_progress(dt):
+    year_days = 366 if dt.is_leap_year() else 365
+    passed_days = dt.timetuple().tm_yday
+    percent = round(((passed_days / year_days) * 100), 2)
+    return percent
+
+def get_month_progress(dt):
+    days_since_start_of_month = dt.day - 1
+    percent = round(((days_since_start_of_month / dt.days_in_month) * 100), 2)
+    return percent
+
+def get_day_progress(dt):
+    percent = round(((dt.hour / 24) * 100), 2)
+    return percent
+
+def make_progress_string(percent):
+    blocks = 16
+    percent = percent * blocks / 100
+    return ''.join(["█" if i < percent else "▁" for i in range(blocks)])
+
 if __name__ == "__main__":
     readme = root / "README.md"
     
     readme_contents = readme.open().read()
+    
+    dt = pendulum.now()
 
+    year_percents = get_year_progress(dt)
+    year_progress = make_progress_string(year_percents)
+    
+    year_progress_text = "\n```text\n⏳ Year progress "+year_progress+" "+year_percents+"%\n```\n"
+    logging.info(code_time_text)
+
+    rewritten = replace_chunk(readme_contents, "year_progress", year_progress_text)
+    
     code_time_text = "\n```text\n"+fetch_code_time().text+"\n```\n"
     logging.info(code_time_text)
 
-    rewritten = replace_chunk(readme_contents, "code_time", code_time_text)
+    rewritten = replace_chunk(rewritten, "code_time", code_time_text)
     logging.info(rewritten)
 
     readme.open("w").write(rewritten)
